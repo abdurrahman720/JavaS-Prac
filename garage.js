aafadae86737a5fcbe0352a02b146906f61642e8
abdurrahman720 <personal.abdurrahman@gmail.com>
2021-11-20 22:00:34 +0600

    updated garage

--- garage.js
+++ garage.js
@@ -7,6 +7,7 @@ class Garage{
     addVehicle(vehicle){
         this.vehicles.push(vehicle); 
         console.log("Vehicle added", vehicle);
+        console.log("It will be released afetr", vehicle.repairTime,"day")
         var isExisting= this.customers[vehicle.owner]; //after adding vehicle, we should put the currentDue as Zero if the vehicle is new in our garage
         if (!isExisting){
             this.customers[vehicle.owner] = 0;
@@ -26,22 +27,23 @@ class Garage{
 
  //we could add vehicle as a single variable but the smarter way is to add this as object so that we can have multiple properties of multiple vehicles
 class Vehicles {
-    constructor(owner,repaircost){
+    constructor(owner,repaircost,repairTime){
         this.owner=owner;
         this.repaircost=repaircost;
+        this.repairTime=repairTime;
     }
 }
 
 //the next two class is not required for Garage but to add different types of vehicles we should extend this class as Bus and Trcuk. However we could also use Vehicle class since the properties are same
 class Bus extends Vehicles{
-    constructor(owner,repaircost,seats){
-        super(owner,repaircost);
+    constructor(owner,repaircost,seats,repairTime){
+        super(owner,repaircost,repairTime);
         this.seats=seats;
     }
 }
 class Truck extends Vehicles{
-    constructor(owner,repaircost,seats){
-        super(owner,repaircost);
+    constructor(owner,repaircost,seats,repairTime){
+        super(owner,repaircost,repairTime);
         this.seats=seats;
         
     }
@@ -49,8 +51,8 @@ class Truck extends Vehicles{
 
 //everything fine! it's time to decalre object as vehicles
 var habluGarage= new Garage ("Bolod Garage Ltd");
-var khulna= new Bus("hanif",40000,45);
-var dhaka= new Truck("Najim",2500,5);
+var khulna= new Bus("hanif",40000,45,7);
+var dhaka= new Truck("Najim",2500,5,10);
 //now use those methods to perform your programming!
 habluGarage.addVehicle(khulna);
 habluGarage.addVehicle(dhaka);
